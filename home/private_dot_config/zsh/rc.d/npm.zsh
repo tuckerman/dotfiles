e156d9650c53386e536d071df5feda0fb3a1b2ec
Cameron Tuckerman-Lee <cameron@ctuck.com>
2026-03-17 13:51:13 -0700

    update

--- home/private_dot_config/zsh/rc.d/npm.zsh
+++ home/private_dot_config/zsh/rc.d/npm.zsh
@@ -1,3 +1,7 @@
 if [ -d "$XDG_DATA_HOME/npm/bin" ]; then
   export PATH="$XDG_DATA_HOME/npm/bin:$PATH"
 fi
+
+if [ -d "$HOME/.npm-global/bin" ]; then
+  export PATH="$HOME/.npm-global/bin:$PATH"
+fi
