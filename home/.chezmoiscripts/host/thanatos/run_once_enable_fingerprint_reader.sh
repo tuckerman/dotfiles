46aae842a1e300363212f864d5652245751915f7
Cameron Tuckerman-Lee <cameron@ctuck.com>
2026-03-18 16:47:10 -0700

    add fingerprint reader support

---
+++ home/.chezmoiscripts/host/thanatos/run_once_enable_fingerprint_reader.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+copr_repo='copr:copr.fedorainfracloud.org:cwt:libfprint-cs9711'
+
+if ! dnf -C repolist enabled | grep -Fq "$copr_repo"; then
+  sudo dnf -y copr enable cwt/libfprint-cs9711
+fi
+
+sudo dnf -y install libfprint-cs9711 --allowerasing
