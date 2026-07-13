7299f13ad1936971e1bf440734a5bcaa932ca821
Cameron Tuckerman-Lee <cameron@ctuck.com>
2026-07-12 20:47:14 -0700

    update

--- home/.chezmoiscripts/os/linux/fedora/run_onchange_configure_keyd.sh
+++ home/.chezmoiscripts/os/linux/fedora/run_onchange_configure_keyd.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 # Version 2026031701
 
-if [ ! -d /etc/keyd ]; then
-  sudo mkdir -p /etc/keyd
-  sudo chown root:keyd /etc/keyd
-  sudo 644 /etc/keyd
-fi
+sudo install -d -o root -g keyd -m 755 /etc/keyd
 
-if [ !  -s /etc/keyd/default.conf ]; then
-  sudo tee /etc/keyd/default.conf << EOM
+sudo tee /etc/keyd/default.conf >/dev/null << EOM
 [ids]
 *
+-1532:02a6:797b7976
 
 [main]
 capslock = overload(capslock, esc)
-leftshift = timeout(overload(leftshift, S-9), 500, layer(leftshift))
-rightshift = timeout(overload(shift, S-0), 500, layer(shift))
+leftshift = overload(leftshift, S-9)
+rightshift = overload(shift, S-0)
 
 [capslock:C]
 h = left
@@ -26,6 +22,27 @@ l = right
 [leftshift:S]
 rightshift = overload(shift, macro(S-9 S-0))
 EOM
-fi
+sudo chmod 644 /etc/keyd/default.conf
+
+sudo tee /etc/keyd/razer-huntsman-v3-pro.conf >/dev/null << EOM
+[ids]
+1532:02a6:797b7976
+
+[main]
+# The keyboard firmware emits leftcontrol for its Caps Lock key.
+leftcontrol = overload(leftcontrol, esc)
+leftshift = overload(leftshift, S-9)
+rightshift = overload(shift, S-0)
+
+[leftcontrol:C]
+h = left
+j = down
+k = up
+l = right
+
+[leftshift:S]
+rightshift = overload(shift, macro(S-9 S-0))
+EOM
+sudo chmod 644 /etc/keyd/razer-huntsman-v3-pro.conf
 
 sudo systemctl restart keyd
